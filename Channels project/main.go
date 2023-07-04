e45228b65efad0d5aeef650d8c00c6c24748f63b
Damian700 <damianpavser@gmail.com>
2023-07-03 23:04:07 -0300

    working on channels

--- Channels project/main.go
+++ Channels project/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
+func main() {
+	links := []string{
+		/* in golang we have to explicit the http protocol before the actual domain */
+		"http://www.google.com",
+		"http://www.facebook.com",
+		"http://www.stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+		"http://aklsdjf.com", /* se pone coma al final tambien */
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println("Error:", err, "not runing properly...")
+		os.Exit(1)
+	}
+	fmt.Println(link, "is up!")
 }
